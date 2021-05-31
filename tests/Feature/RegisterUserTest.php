9f398373f65401c8695762f9b62d814d0a344912
d.shaklin <d.shaklin@telecontact.ru>
2021-05-31 18:06:44 +0300

    -Написал второй тест для проверки успешности регистрации пользователя

--- tests/Feature/RegisterUserTest.php
+++ tests/Feature/RegisterUserTest.php
@@ -14,7 +14,7 @@ class RegisterUserTest extends TestCase
      *
      * @return void
      */
-    public function testRegisterUser()
+    public function test_success_of_user_registration()
     {
         $login = 'test_login';
         $email = 'test_email';
@@ -31,4 +31,40 @@ class RegisterUserTest extends TestCase
 
         $this->assertEquals($login, $user->login);
     }
+
+    /**
+     * Тест на проверку того что пользователь с тем же логином не может зарегестрироваться
+     */
+    public function test_registration_of_a_user_with_a_duplicate_login_fails(){
+        $login1 = 'test_login_1';
+        $email1 = 'test_email_1';
+        $password1 = 'test_password_1';
+
+        $response1 = $this->post('/api/register', array(
+            'login' => $login1,
+            'email' => $email1,
+            'password' => $password1
+        ));
+
+        $login2 = 'test_login_1';
+        $email2 = 'test_email_2';
+        $password2 = 'test_password_2';
+
+        $response2 = $this->post('/api/register', array(
+            'login' => $login2,
+            'email' => $email2,
+            'password' => $password2
+        ));
+
+        $response2->assertStatus(422)->assertJson(
+            [
+                "error" => [
+                    "code" => 422,
+                    "message" => "User with this login already exists"
+                ]
+            ]
+        );
+
+    }
+
 }
